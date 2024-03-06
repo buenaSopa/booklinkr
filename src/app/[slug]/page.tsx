e8e99529766170442b7780aec61b4fe317cf4206
bryan <bryankoh17102@gmail.com>
2024-03-06 22:52:57 +0800

    copy to clipboard slug

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -4,7 +4,7 @@ import HeaderSlug from "@/components/ui/header-slug"
 export default function Page({ params }: { params: { slug: string } }) {
 	return (
 		<section className="text-2xl">
-			<HeaderSlug params={"fkkc"}/>
+			<HeaderSlug shelf={params.slug}/>
 			<div>My Post: {params.slug}</div>
 		</section>
 
--- src/components/ui/header-slug.tsx
+++ src/components/ui/header-slug.tsx
@@ -1,16 +1,26 @@
 "use client"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Button } from "./button";
+import { IconCopy } from "@tabler/icons-react";
 
 export default function HeaderSlug({ shelf }: { shelf: string }) {
 
+	const [copied, setCopied] = useState(false)
 
 	return (
 		<>
 			<div className="flex bg-darkgreen text-lightbrown justify-between sm:px-10 py-3 items-center px-2">
 				<div>
-					copy slug
+					<CopyToClipboard text={`https://www.booklinkr.com/${shelf}`}
+						onCopy={() => setCopied(true)}>
+						<Button className="text-2xl border-lightbrown text-darkgreen bg-lightbrown border-2 rounded-full gap-2 hover:text-lightbrown">
+							{shelf}
+							<IconCopy className="h-4 w-4"/>
+						</Button>
+					</CopyToClipboard>
 				</div>
 				<div>
 					<Avatar>
